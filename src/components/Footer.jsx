4a0ad8c4309b3da0dcb5c550ab80da8ef6f90cd6
Precious <precious.soriano@ynov.com>
2024-09-04 20:04:31 +0200

    footer update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -84,14 +84,9 @@ const Footer = () => {
       </div>
 
       <div className="container mx-auto mt-8">
-        <hr className="my-4"/>
-        <div className="flex justify-between items-center text-center sm:text-left">
+        <hr className="my-4" />
+        <div className="flex justify-center sm:justify-between items-center text-center sm:text-left">
           <p className="pt-2 font-semibold">&copy; 2024 Palworld E-shop. Tous droits réservés.</p>
-          <div className="flex space-x-4">
-            <FaFacebook className="text-gray-600 hover:text-blue-600 transition-colors duration-300 text-xl" />
-            <FaTwitter className="text-gray-600 hover:text-blue-400 transition-colors duration-300 text-xl" />
-            <FaInstagram className="text-gray-600 hover:text-pink-600 transition-colors duration-300 text-xl" />
-          </div>
         </div>
       </div>
     </footer>
